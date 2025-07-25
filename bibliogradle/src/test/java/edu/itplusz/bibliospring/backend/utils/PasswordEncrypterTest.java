410d581ef25f5d88393c96093b5e5eceb2426e25
KincsoV <kincso.gabriella.varga@stud.ubbcluj.ro>
2025-07-25 11:05:51 +0300

    test passed for hashingPassword

--- bibliogradle/src/test/java/edu/itplusz/bibliospring/backend/utils/PasswordEncrypterTest.java
+++ bibliogradle/src/test/java/edu/itplusz/bibliospring/backend/utils/PasswordEncrypterTest.java
@@ -9,16 +9,15 @@ import static org.assertj.core.api.Assertions.*;
 class PasswordEncrypterTest {
 
     @Test
-    public void passwordAndSalt_HashingPassword_PasswordIsHashed(){
+    public void passwordAndSalt_hashingPassword_passwordIsHashed(){
         //given//arrange
         PasswordEncrypter sut = new PasswordEncrypter(); //system under tester
         String password = "password";
-        String salt = UUID.randomUUID().toString();
+        String salt = "ebddeb7b-32fb-4f51-9743-190bedfc0e7f";
         //when//act
-        sut.hashPassword(password, salt);
         String hash = sut.hashPassword(password, salt);
         //then//assert
-        String expectedHash="123456";
+        String expectedHash="BC50CD2F0BC9EE55739E2EC419FF45C6F7E160A64A23136A9A171F9AD7B19CF6";
         assertThat(hash).isEqualTo(expectedHash);
     }
 
